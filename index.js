6229f8cbcc19abd09367bd689348be82dc423ab7
Marcus <marcus.kalmar@gmail.com>
2019-11-18 15:14:41 +0100

    Setup config and two simple routes

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { pool } = require('./config')
 
-app = express()
 const PORT = 3002
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const getGames = (request, response) => {
+    pool.query('SELECT * FROM Games', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+const addGame = (request, response) => {
+  const { title, genre, developer_studio, release_date, metacritic_score, steam_review, publisher } = request.body
+
+  pool.query(
+    'INSERT INTO books (title, genre, developer_studio, release_date, metacritic_score, steam_review, publisher) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+    [title, genre, developer_studio, release_date, metacritic_score, steam_review, publisher], error => {
+        if (error) {
+        throw error
+        }
+        response.status(201).json({ status: 'success', message: 'Game added.' })
+    })
+}
+
+app
+  .route('/books')
+  .get(getGames)
+  .post(addGame)
 
 app.listen(PORT, () => console.log(`The Server is up and running on port ${PORT}`))
 
 app.get(`/`, (request, response) => {
-    response.send('This is the first test response! from the home route: /');
+    response.send('This is the first test response! from the home route: /')
   });
